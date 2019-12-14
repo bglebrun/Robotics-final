8dddf9098e66aeea74a2a370e5aacfe2139d5c70
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-12-14 08:42:35 +0000

    Left and right split

--- main.py
+++ main.py
@@ -5,52 +5,84 @@ returns a simplified lane detection algorithm"""
 import numpy as np
 import cv2
 
+def EXTRACT_KEYFRAME(FRAME):
+    LOWER_THRESH = 55
+    UPPER_THRESH = 255
+    RET, BINARY = cv2.threshold(FRAME, LOWER_THRESH, UPPER_THRESH, cv2.THRESH_BINARY_INV)
+    # EDGES = cv2.Canny(BINARY, 100, 200)
+
+    # Gaussian blur for filtering
+    BLUR = cv2.GaussianBlur(BINARY, (5, 5), 0)
+
+    return BLUR
+
+def BUILD_BLOB_DETECTOR():
+    # Setup SimpleBlobDetector parameters.
+    params = cv2.SimpleBlobDetector_Params()
+    
+    # Change thresholds
+    params.minThreshold = 10
+    params.maxThreshold = 255
+    
+    # Filter by Area.
+#    params.filterByArea = True
+#    params.minArea = 1
+#    params.maxArea = 100
+    
+    # Filter by Circularity
+    # params.filterByCircularity = True
+    # params.minCircularity = 0.1
+    
+    # Filter by Convexity
+    # params.filterByConvexity = True
+    # params.minConvexity = 0.87
+    
+    # Filter by Inertia
+    #  params.filterByInertia = True
+    # params.minInertiaRatio = 0.01
+    
+    # Create a detector with the parameters
+    ver = (cv2.__version__).split('.')
+    if int(ver[0]) < 3 :
+        detector = cv2.SimpleBlobDetector(params)
+    else : 
+        detector = cv2.SimpleBlobDetector_create(params)
+    
+    return detector
+
+
 # Initialize video capture
 VIDEO_CAPTURE = cv2.VideoCapture(0)
 
+
 # While video capture is running
 while VIDEO_CAPTURE.isOpened():
+
     # Get capture frame
     RET, FRAME = VIDEO_CAPTURE.read()
 
-    # Grayscale and invert
-    GRAY_IMG = cv2.cvtColor(FRAME, cv2.COLOR_BGR2GRAY)
-    INV_GRAY = cv2.bitwise_not(GRAY_IMG)
+    if not RET:
+        break
 
-    # HSV for yellow masking
-    HSV = cv2.cvtColor(FRAME, cv2.COLOR_BGR2HSV)
+    # Split frame into left and right
+    FRAME_RIGHT = FRAME[0:544, 480:960]
+    FRAME_LEFT = FRAME[0:544, 0:480]
 
-    # Define Yellow-white mask
-    LOWER_YELLOW = np.array([20, 100, 100], dtype="uint8")
-    UPPER_YELLOW = np.array([30, 255, 255], dtype="uint8")
+    # Logitech camera actual resolution: 960 x 544, 0 starts at top left
+    ROI = FRAME[136:544, 0:960]
 
-    # Apply masks for yellow-white detection
-    MASK_YELLOW = cv2.inRange(HSV, LOWER_YELLOW, UPPER_YELLOW)
-    MASK_WHITE = cv2.inRange(INV_GRAY, 200, 255)
-    MASK_YW = cv2.bitwise_or(MASK_WHITE, MASK_YELLOW)
-    MASK_YW_IMAGE = cv2.bitwise_and(INV_GRAY, MASK_YW)
+    # TODO: Old process code, change for left and right frames
+    PROCESSED = EXTRACT_KEYFRAME(ROI)
 
-    # Gaussian blur for filtering
-    BLUR = cv2.GaussianBlur(MASK_YW_IMAGE, (5, 5), 0)
+    DETECTOR = BUILD_BLOB_DETECTOR()
 
-    # Canny edge detection threshold, try 1:2 or 1:3
-    LOW_THRESH = 50
-    HIGH_THRESH = 150
+    # Detect Blobs
+    KEYPOINTS = DETECTOR.detect(PROCESSED)
 
-    CANNY_EDGE = cv2.Canny(BLUR, LOW_THRESH, HIGH_THRESH)
-
-    LINES = cv2.HoughLinesP(CANNY_EDGE, 1, np.pi/180, 200)
-
-    hough_calc = np.copy(FRAME) * 0
-
-    for line in LINES:
-        for x1, y1, x2, y2 in line:
-            cv2.line(hough_calc, (x1, y1), (x2, y2), (255, 0, 0), 10)
-
-    FINAL = cv2.addWeighted(FRAME, 0.8, hough_calc, 1, 0)
+    IMAGE_KEYPOINTS = cv2.drawKeypoints(PROCESSED, KEYPOINTS, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # Display the resulting processed frame
-    cv2.imshow('FRAME', FINAL)
+    cv2.imshow('FRAME', IMAGE_KEYPOINTS)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
