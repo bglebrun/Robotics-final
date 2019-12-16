a66d9d2e97d3895ebd803e4b7f79be6e782d308d
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-12-15 17:57:58 -0700

    GUI work

--- main.py
+++ main.py
@@ -5,41 +5,51 @@ returns a simplified lane detection algorithm"""
 import numpy as np
 import cv2
 
-def EXTRACT_KEYFRAME(FRAME, LOWER_THRESH=175, inverted=False):
-    UPPER_THRESH = 255
+def extract_keyframe(image, lower_thresh=175, inverted=False):
+    """Processes and returns frames for white space counting"""
+    upper_thresh = 255
     if inverted:
-        TYPE = cv2.THRESH_BINARY_INV
+        thresh_type = cv2.THRESH_BINARY_INV
     else:
-        TYPE = cv2.THRESH_BINARY
-    GREY = cv2.cvtColor(FRAME, cv2.COLOR_BGR2GRAY)
-    RET, BINARY = cv2.threshold(GREY, LOWER_THRESH, UPPER_THRESH, TYPE)
-    # EDGES = cv2.Canny(BINARY, 100, 200)
+        thresh_type = cv2.THRESH_BINARY
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, binary = cv2.threshold(grey, lower_thresh, upper_thresh, thresh_type)
+    # EDGES = cv2.Canny(binary, 100, 200)
 
-    # Gaussian blur for filtering
-    BLUR = cv2.GaussianBlur(BINARY, (5, 5), 0)
+    # Gaussian blur for "filtering"
+    blur = cv2.GaussianBlur(binary, (5, 5), 0)
 
-    return BLUR
+    return blur
 
-def CONTROLLER(LHS, RHS, DEADZONE = 6000):
-    pos = RHS - LHS
-    # TODO Update comments
+def controller(lhs, rhs, deadzone_val=6000):
+    """Processes correct turn direction based on white values"""
+    pos = rhs - lhs
+
     #if the pos is positive we are too far right, turn left
-    if pos < -1*DEADZONE:
+    if pos < -1*deadzone_val:
         print("turn left")
 
     #if the pos is negative we are too far left, turn right
-    elif pos > DEADZONE :
+    elif pos > deadzone_val:
         print("turn right")
 
     #if pos == 0 we are dead center drive straight
     else:
         print("go straight")
 
-    return
-
 # Initialize video capture
 VIDEO_CAPTURE = cv2.VideoCapture(0)
 
+SWITCH = '0 : NORM \n1 : INV'
+INV = False
+
+# Create trackbars for value editing
+cv2.createTrackbar('threshold', 'FRAME', 175, 255)
+cv2.createTrackbar(SWITCH, 'FRAME', 0, 1)
+cv2.createTrackbar('deadzone', 'FRAME', 6000, 100000)
+
+THRESHOLD = 0
+DEADZONE = 0
 
 # While video capture is running
 while VIDEO_CAPTURE.isOpened():
@@ -53,20 +63,30 @@ while VIDEO_CAPTURE.isOpened():
     # Logitech camera actual resolution: 960 x 544, 0 starts at top left
     ROI = FRAME[144:544, 0:960]
 
-    PROCESSED = EXTRACT_KEYFRAME(FRAME)
+    PROCESSED = extract_keyframe(FRAME, THRESHOLD, INV)
 
     # Split frame into left and right
     FRAME_RIGHT = PROCESSED[144:544, 480:960]
     FRAME_LEFT = PROCESSED[144:544, 0:480]
 
-    whiteLHS = np.sum(FRAME_LEFT == 255)
-    print('Number of white pixels LHS:', whiteLHS)
+    LHS_WHITE = np.sum(FRAME_LEFT == 255)
+    print('Number of white pixels LHS:', LHS_WHITE)
 
-    whiteRHS = np.sum(FRAME_RIGHT == 255)
+    RHS_WHITE = np.sum(FRAME_RIGHT == 255)
 
-    print("Number of white pixles RHS: ", whiteRHS)
+    print("Number of white pixles RHS: ", RHS_WHITE)
 
-    CONTROLLER(whiteLHS, whiteRHS)
+    controller(LHS_WHITE, RHS_WHITE, DEADZONE)
+
+    THRESHOLD = cv2.getTrackbarPos('threshold', 'FRAME')
+    S_VAL = cv2.getTrackbarPos(SWITCH, 'FRAME')
+    DEADZONE = cv2.getTrackbarPos('deadzone', 'FRAME')
+
+    if S_VAL == 0:
+        INV = False
+    else:
+        INV = True
+
 
     # Display the resulting processed frame
     cv2.imshow('FRAME', PROCESSED)
